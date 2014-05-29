0ded8ce4770c6da18a98a8bb721d9e80fd1c52da
Mohamed KEITA <medesko@gmail.com>
2014-05-29 23:02:47 +0200

    citybox plugin

--- wp_citybox.php
+++ wp_citybox.php
@@ -22,17 +22,18 @@ if ( is_admin() ) {
 class mycityBox {
 
 	public function __construct() {
-		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
-		add_action( 'save_post', array( $this, 'save_metabox' ) );
+		add_action( 'add_meta_boxes', array( $this, 'addMetabox' ) );
+		add_action( 'save_post', array( $this, 'saveMetabox' ) );
+		add_shortcode( 'citybox', 'cityBox' );
 	}
 
-	public function add_meta_box( $post_type ) {
+	public function addMetabox( $post_type ) {
             $post_types = array('post', 'page');
             if ( in_array( $post_type, $post_types )) {
 				add_meta_box(
 					'meta_citybox'
 					,__( 'Votre ville')
-					,array( $this, 'render_meta_citybox' )
+					,array( $this, 'MetaCitybox' )
 					,$post_type
 					,'advanced'
 					,'high'
@@ -41,7 +42,7 @@ class mycityBox {
 	}
 
 	/* Rendering the Meta Box */
-	public function render_meta_citybox( $post ) {
+	public function MetaCitybox( $post ) {
 		$value = get_post_meta( $post->ID, '_city_box', true );
 		
 		echo '<input type="text" id="city_box" name="city_box" value="' . esc_attr( $value ) . '" />';
@@ -55,7 +56,7 @@ class mycityBox {
 	}
 
 	/* Saving the meta data. */
-	public function save_metabox($post_id){
+	public function saveMetabox($post_id){
 		// If this is an autosave, our form has not been submitted,
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
 			return $post_id;
@@ -75,6 +76,14 @@ class mycityBox {
 		if(isset($_POST['city_box']))
 		  update_post_meta($post_id, '_city_box', $_POST['city_box']);
 
+	}
+
+	public function cityBox() {
+	  global $post;
+	  $city = get_post_meta( $post->ID, '_city_box', true );
+	  var_dump($city);
+
+      return  $city;
 	}
 
 }
