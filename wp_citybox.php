115c6a9956ca39d847089d2fe7cfddd77278b255
Mohamed KEITA <medesko@gmail.com>
2014-05-31 20:33:42 +0200

    update sql request

--- wp_citybox.php
+++ wp_citybox.php
@@ -9,53 +9,23 @@
  * License: Free
  */
 
-/* Calls the class on the post edit screen. */
-function cityBox() {
-    new mycityBox();
-}
 //add the meta box to our post screen would look like this:
-if ( is_admin() ) {
-    add_action( 'load-post.php', 'cityBox' );
-    add_action( 'load-post-new.php', 'cityBox' );
+add_action('add_meta_boxes','init_metabox');
+function init_metabox(){
+  add_meta_box('Metacitybox', 'Your city', 'MetaCitybox', 'post', 'side');
 }
 
-class mycityBox {
+/* Rendering the Meta Box */
+function MetaCitybox($post){
+  $city = get_post_meta($post->ID,'_city_box',true);
+  echo '<label for="city_box">City name:</label>';
+  echo '<input id="city_box" type="text" name="city_box" value="'.$city.'" />';
+}
 
-	public function __construct() {
-		add_action( 'add_meta_boxes', array( $this, 'addMetabox' ) );
-		add_action( 'save_post', array( $this, 'saveMetabox' ) );
-	}
-
-	public function addMetabox( $post_type ) {
-            $post_types = array('post', 'page');
-            if ( in_array( $post_type, $post_types )) {
-				add_meta_box(
-					'meta_citybox'
-					,__( 'Votre ville')
-					,array( $this, 'MetaCitybox' )
-					,$post_type
-					,'advanced'
-					,'high'
-				);
-            }
-	}
-
-	/* Rendering the Meta Box */
-	public function MetaCitybox( $post ) {
-		$city = get_post_meta( $post->ID, '_city_box', true );
-		echo '<input type="text" id="city_box" name="city_box" value="' . esc_attr( $city ) . '" />';
-		// echo '<label for="city_box">Selectionnez votre ville : </label>';
-		// echo '<select name="city_box">';
-		// echo '<option ' . selected( 'paris', $value, false ) . ' value="paris">Paris</option>';
-		// echo '<option ' . selected( 'marseille', $value, false ) . ' value="marseille">Marseille</option>';
-		// echo '<option ' . selected( 'lille', $value, false ) . ' value="lille">Lille</option>';
-		// echo '</select>';
-
-	}
-
-	/* Saving the meta data. */
-	public function saveMetabox($post_id){
-		// If this is an autosave, our form has not been submitted,
+/* Saving the meta data. */
+add_action('save_post','save_metabox');
+function save_metabox($post_id){
+		//If this is an autosave, our form has not been submitted,
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
 			return $post_id;
 
@@ -64,18 +34,15 @@ class mycityBox {
 
 			if ( ! current_user_can( 'edit_page', $post_id ) )
 				return $post_id;
-		
 		} else {
 
 			if ( ! current_user_can( 'edit_post', $post_id ) )
 				return $post_id;
 		}
-		// Update the meta field.
-		if(isset($_POST['city_box']))
-		  update_post_meta($post_id, '_city_box', $_POST['city_box']);
-
-	}
+if(isset($_POST['city_box']))
+  update_post_meta($post_id, '_city_box', esc_html($_POST['city_box']));
 }
+
 // shortcode hook : get city value
 add_shortcode('city', 'cityShortcode');
 function cityShortcode($atts, $content = null) {
@@ -83,5 +50,36 @@ function cityShortcode($atts, $content = null) {
 	$city = get_post_meta( $post->ID, '_city_box', true );
 	return $city;
 }
+
+// -- search_filter --
+function search_filter($query) {
+  if ( !is_admin() && $query->is_search() ) {
+  $s = get_search_query();
+  global $wpdb;
+  $querystr = "
+        SELECT $wpdb->posts.*
+        FROM $wpdb->posts INNER JOIN $wpdb->postmeta 
+        ON ($wpdb->posts.ID = $wpdb->postmeta.post_id) 
+        WHERE 1=1 AND ((($wpdb->posts.post_title LIKE '%$s%') 
+        OR ($wpdb->posts.post_content LIKE '%$s%'))) 
+        AND $wpdb->posts.post_type = 'post' 
+        OR ($wpdb->posts.post_status = 'publish') 
+        AND ( ($wpdb->postmeta.meta_key = '_city_box' 
+        AND CAST($wpdb->postmeta.meta_value AS CHAR) 
+        LIKE '%$s%') ) GROUP BY $wpdb->posts.ID 
+        ORDER BY $wpdb->posts.post_title 
+        LIKE '%$s%' DESC, 
+        $wpdb->posts.post_date 
+        DESC";
+      // echo $querystr;
+      $pageposts = $wpdb->get_results($querystr, OBJECT);
+      var_dump($pageposts);
+      // $query->set('post_type', $pageposts[0]->post_type);
+
+    }
+}
+add_action('pre_get_posts','search_filter');
+
+
 ?>
 
